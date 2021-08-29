8729f84aef1fe25685e5992dc2aa65f3bd0bf990
Mann mit Hut <github+mmhat@with-h.at>
2021-08-29 13:38:56 +0200

    Renamed `Process` effect to `TypedProcess`

--- effectful-typed-process/src/Effectful/Process/Typed.hs
+++ effectful-typed-process/src/Effectful/Process/Typed.hs
@@ -1,7 +1,7 @@
 module Effectful.Process.Typed
   ( -- * Process effect
-    Process
-  , runProcessE
+    TypedProcess
+  , runTypedProcess
 
   -- * Launch a process
   , startProcess
@@ -35,110 +35,110 @@ import Effectful.Internal.Effect
 import Effectful.Internal.Monad
 
 -- | An effect for running child processes using the @typed-process@ library.
-data Process :: Effect where
-    Process :: Process m r
+data TypedProcess :: Effect where
+    TypedProcess :: TypedProcess m r
 
-runProcessE :: IOE :> es => Eff (Process : es) a -> Eff es a
-runProcessE = evalEffect (IdE Process)
+runTypedProcess :: IOE :> es => Eff (TypedProcess : es) a -> Eff es a
+runTypedProcess = evalEffect (IdE TypedProcess)
 
 ----------------------------------------
 -- Launch a process
 
 -- | Lifted 'PT.startProcess'.
-startProcess :: Process :> es => PT.ProcessConfig stdin stdout stderr -> Eff es (PT.Process stdin stdout stderr)
+startProcess :: TypedProcess :> es => PT.ProcessConfig stdin stdout stderr -> Eff es (PT.Process stdin stdout stderr)
 startProcess = unsafeEff_ . PT.startProcess
 
 -- | Lifted 'PT.stopProcess'.
-stopProcess :: Process :> es => PT.Process stdin stdout stderr -> Eff es ()
+stopProcess :: TypedProcess :> es => PT.Process stdin stdout stderr -> Eff es ()
 stopProcess = unsafeEff_ . PT.stopProcess
 
 -- | Lifted 'PT.withProcessWait'.
-withProcessWait :: Process :> es => PT.ProcessConfig stdin stdout stderr -> (PT.Process stdin stdout stderr -> Eff es a) -> Eff es a
+withProcessWait :: TypedProcess :> es => PT.ProcessConfig stdin stdout stderr -> (PT.Process stdin stdout stderr -> Eff es a) -> Eff es a
 withProcessWait = liftWithProcess PT.withProcessWait
 
 -- | Lifted 'PT.withProcessWait_'.
-withProcessWait_ :: Process :> es => PT.ProcessConfig stdin stdout stderr -> (PT.Process stdin stdout stderr -> Eff es a) -> Eff es a
+withProcessWait_ :: TypedProcess :> es => PT.ProcessConfig stdin stdout stderr -> (PT.Process stdin stdout stderr -> Eff es a) -> Eff es a
 withProcessWait_ = liftWithProcess PT.withProcessWait_
 
 -- | Lifted 'PT.withProcessTerm'.
-withProcessTerm :: Process :> es => PT.ProcessConfig stdin stdout stderr -> (PT.Process stdin stdout stderr -> Eff es a) -> Eff es a
+withProcessTerm :: TypedProcess :> es => PT.ProcessConfig stdin stdout stderr -> (PT.Process stdin stdout stderr -> Eff es a) -> Eff es a
 withProcessTerm = liftWithProcess PT.withProcessTerm
 
 -- | Lifted 'PT.withProcessTerm_'.
-withProcessTerm_ :: Process :> es => PT.ProcessConfig stdin stdout stderr -> (PT.Process stdin stdout stderr -> Eff es a) -> Eff es a
+withProcessTerm_ :: TypedProcess :> es => PT.ProcessConfig stdin stdout stderr -> (PT.Process stdin stdout stderr -> Eff es a) -> Eff es a
 withProcessTerm_ = liftWithProcess PT.withProcessTerm_
 
 -- | Lifted 'PT.readProcess'.
-readProcess :: Process :> es => PT.ProcessConfig stdin stdoutIgnored stderrIgnored -> Eff es (ExitCode, ByteString, ByteString)
+readProcess :: TypedProcess :> es => PT.ProcessConfig stdin stdoutIgnored stderrIgnored -> Eff es (ExitCode, ByteString, ByteString)
 readProcess = unsafeEff_ . PT.readProcess
 
 -- | Lifted 'PT.readProcess_'.
-readProcess_ :: Process :> es => PT.ProcessConfig stdin stdoutIgnored stderrIgnored -> Eff es (ByteString, ByteString)
+readProcess_ :: TypedProcess :> es => PT.ProcessConfig stdin stdoutIgnored stderrIgnored -> Eff es (ByteString, ByteString)
 readProcess_ = unsafeEff_ . PT.readProcess_
 
 -- | Lifted 'PT.runProcess'.
-runProcess :: Process :> es => PT.ProcessConfig stdin stdout stderr -> Eff es ExitCode
+runProcess :: TypedProcess :> es => PT.ProcessConfig stdin stdout stderr -> Eff es ExitCode
 runProcess = unsafeEff_ . PT.runProcess
 
 -- | Lifted 'PT.runProcess_'.
-runProcess_ :: Process :> es => PT.ProcessConfig stdin stdout stderr -> Eff es ()
+runProcess_ :: TypedProcess :> es => PT.ProcessConfig stdin stdout stderr -> Eff es ()
 runProcess_ = unsafeEff_ . PT.runProcess_
 
 -- | Lifted 'PT.runProcessStdout'.
-runProcessStdout :: Process :> es => PT.ProcessConfig stdin stdoutIgnored stderr -> Eff es (ExitCode, ByteString)
+runProcessStdout :: TypedProcess :> es => PT.ProcessConfig stdin stdoutIgnored stderr -> Eff es (ExitCode, ByteString)
 runProcessStdout = unsafeEff_ . PT.readProcessStdout
 
 -- | Lifted 'PT.runProcessStdout_'.
-runProcessStdout_ :: Process :> es => PT.ProcessConfig stdin stdoutIgnored stderr -> Eff es ByteString
+runProcessStdout_ :: TypedProcess :> es => PT.ProcessConfig stdin stdoutIgnored stderr -> Eff es ByteString
 runProcessStdout_ = unsafeEff_ . PT.readProcessStdout_
 
 -- | Lifted 'PT.runProcessStderr'.
-runProcessStderr :: Process :> es => PT.ProcessConfig stdin stdout stderrIgnored -> Eff es (ExitCode, ByteString)
+runProcessStderr :: TypedProcess :> es => PT.ProcessConfig stdin stdout stderrIgnored -> Eff es (ExitCode, ByteString)
 runProcessStderr = unsafeEff_ . PT.readProcessStderr
 
 -- | Lifted 'PT.runProcessStderr_'.
-runProcessStderr_ :: Process :> es => PT.ProcessConfig stdin stdout stderrIgnored -> Eff es ByteString
+runProcessStderr_ :: TypedProcess :> es => PT.ProcessConfig stdin stdout stderrIgnored -> Eff es ByteString
 runProcessStderr_ = unsafeEff_ . PT.readProcessStderr_
 
 -- | Lifted 'PT.runProcessInterleaved'.
-runProcessInterleaved :: Process :> es => PT.ProcessConfig stdin stdoutIgnored stderrIgnored -> Eff es (ExitCode, ByteString)
+runProcessInterleaved :: TypedProcess :> es => PT.ProcessConfig stdin stdoutIgnored stderrIgnored -> Eff es (ExitCode, ByteString)
 runProcessInterleaved = unsafeEff_ . PT.readProcessInterleaved
 
 -- | Lifted 'PT.runProcessInterleaved_'.
-runProcessInterleaved_ :: Process :> es => PT.ProcessConfig stdin stdoutIgnored stderrIgnored -> Eff es ByteString
+runProcessInterleaved_ :: TypedProcess :> es => PT.ProcessConfig stdin stdoutIgnored stderrIgnored -> Eff es ByteString
 runProcessInterleaved_ = unsafeEff_ . PT.readProcessInterleaved_
 
 ----------------------------------------
 -- Process exit code
 
 -- | Lifted 'PT.waitExitCode'.
-waitExitCode :: Process :> es => PT.Process stdin stdout stderr -> Eff es ExitCode
+waitExitCode :: TypedProcess :> es => PT.Process stdin stdout stderr -> Eff es ExitCode
 waitExitCode = unsafeEff_ . PT.waitExitCode
 
 -- | Lifted 'PT.waitExitCodeSTM'.
---waitExitCodeSTM :: Process :> es => PT.Process stdin stdout stderr -> Eff es ExitCode
+--waitExitCodeSTM :: TypedProcess :> es => PT.Process stdin stdout stderr -> Eff es ExitCode
 --waitExitCodeSTM = unsafeEff_ . PT.waitExitCode
 
 -- | Lifted 'PT.getExitCode'.
-getExitCode :: Process :> es => PT.Process stdin stdout stderr -> Eff es (Maybe ExitCode)
+getExitCode :: TypedProcess :> es => PT.Process stdin stdout stderr -> Eff es (Maybe ExitCode)
 getExitCode = unsafeEff_ . PT.getExitCode
 
 -- | Lifted 'PT.getExitCodeSTM'.
---getExitCodeSTM :: Process :> es => PT.Process stdin stdout stderr -> Eff es (Maybe ExitCode)
+--getExitCodeSTM :: TypedProcess :> es => PT.Process stdin stdout stderr -> Eff es (Maybe ExitCode)
 --getExitCodeSTM = unsafeEff_ . PT.getExitCodeSTM
 
 -- | Lifted 'PT.checkExitCode'.
-checkExitCode :: Process :> es => PT.Process stdin stdout stderr -> Eff es ()
+checkExitCode :: TypedProcess :> es => PT.Process stdin stdout stderr -> Eff es ()
 checkExitCode = unsafeEff_ . PT.checkExitCode
 
 -- | Lifted 'PT.checkExitCodeSTM'.
---checkExitCodeSTM :: Process :> es => PT.Process stdin stdout stderr -> Eff es ()
+--checkExitCodeSTM :: TypedProcess :> es => PT.Process stdin stdout stderr -> Eff es ()
 --checkExitCodeSTM = unsafeEff_ . PT.checkExitCodeSTM
 
 ----------------------------------------
 -- Helpers
 
-liftWithProcess :: Process :> es => (PT.ProcessConfig stdin stdout stderr -> (PT.Process stdin stdout stderr -> IO a) -> IO a) -> PT.ProcessConfig stdin stdout stderr -> (PT.Process stdin stdout stderr -> Eff es a) -> Eff es a
+liftWithProcess :: TypedProcess :> es => (PT.ProcessConfig stdin stdout stderr -> (PT.Process stdin stdout stderr -> IO a) -> IO a) -> PT.ProcessConfig stdin stdout stderr -> (PT.Process stdin stdout stderr -> Eff es a) -> Eff es a
 liftWithProcess k pc f = unsafeEff $ \es ->
   unsafeSeqUnliftEff es $ \runInIO ->
     k pc (runInIO . f)
