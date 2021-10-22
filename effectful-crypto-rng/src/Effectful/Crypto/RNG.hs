4f106e29d640d9098b44c4ecefb561e0c4ec881e
Mann mit Hut <github+mmhat@with-h.at>
2021-10-22 22:13:20 +0200

    Added statically dispatched version of CryptoRNG effect

---
+++ effectful-crypto-rng/src/Effectful/Crypto/RNG.hs
@@ -0,0 +1,79 @@
+{-# OPTIONS_GHC -fno-warn-redundant-constraints #-}
+{-|
+  Module      : Effectful.Crypto.RNG
+  Copyright   : © Hécate Moonlight, 2021
+                  Dominik Peteler, 2021
+  License     : MIT
+  Maintainer  : hecate@glitchbra.in
+  Stability   : stable
+
+  An effect wrapper around Crypto.RNG for the Effectful ecosystem
+-}
+module Effectful.Crypto.RNG
+  ( -- * CryptoRNG Effect
+    CryptoRNG(..)
+
+    -- * Runner
+  , runCryptoRNG
+
+    -- * CryptorRNG functions
+  , CryptoRNGState
+  , randomString
+  , randomBytes
+  , randomR
+  , newCryptoRNGState
+  , unsafeCryptoRNGState
+
+    -- * Re-exports from Crypto.RNG
+  , C.mapCryptoRNGT
+  , C.runCryptoRNGT
+  , C.withCryptoRNGState
+  ) where
+
+import Crypto.Classes (ByteLength)
+import Crypto.RNG (CryptoRNGState)
+import Data.ByteString (ByteString)
+import Effectful.Internal.Effect
+import Effectful.Internal.Env
+import Effectful.Internal.Monad
+import qualified Crypto.RNG as C
+import qualified Crypto.RNG.Utils as C
+
+-- | An effect for the cryptographic random generator provided by the DRBG package.
+data CryptoRNG :: Effect where
+  CryptoRNG :: CryptoRNGState -> CryptoRNG m a
+
+-- | The default Effect handler
+runCryptoRNG
+  :: IOE :> es
+  => CryptoRNGState
+  -> Eff (CryptoRNG : es) a
+  -> Eff es a
+runCryptoRNG rngState = evalEffect (IdE (CryptoRNG rngState))
+
+-- | Create a new 'CryptoRNGState', based on system entropy.
+newCryptoRNGState :: IOE :> es => Eff es CryptoRNGState
+newCryptoRNGState = C.newCryptoRNGState
+
+-- | Create a new 'CryptoRNGState', based on a bytestring seed.
+-- Should only be used for testing.
+unsafeCryptoRNGState :: IOE :> es => ByteString -> Eff es CryptoRNGState
+unsafeCryptoRNGState seed = C.unsafeCryptoRNGState seed
+
+-- | Generate given number of cryptographically secure random bytes.
+randomBytes :: CryptoRNG :> es => ByteLength -> Eff es ByteString
+randomBytes len = unsafeEff $ \es -> do
+  IdE (CryptoRNG rngState) <- getEnv es
+  C.randomBytesIO len rngState
+
+-- | Generate random string of specified length that contains allowed chars.
+randomString :: CryptoRNG :> es => Int -> String -> Eff es String
+randomString len allowedChars = unsafeEff $ \es -> do
+  IdE (CryptoRNG rngState) <- getEnv es
+  C.runCryptoRNGT rngState (C.randomString len allowedChars)
+
+-- | Generate a cryptographically secure random number in given, closed range.
+randomR :: (CryptoRNG :> es, Integral a) => (a, a) -> Eff es a
+randomR (low, high) = unsafeEff $ \es -> do
+  IdE (CryptoRNG rngState) <- getEnv es
+  C.runCryptoRNGT rngState $ C.randomR (low, high)
--- effectful-crypto-rng/test/Main.hs
+++ effectful-crypto-rng/test/Main.hs
@@ -1,47 +1,15 @@
-module Main where
+module Main (main) where
 
-import Control.Monad
-import Data.ByteString
-import Effectful.Monad
-import Effectful.Reader
-import Test.Hspec as H
+import Test.Hspec
 
-import Effectful.Crypto.RNG.Dynamic
+import qualified Dynamic
+import qualified Static
 
 main :: IO ()
 main = hspec spec
 
 spec :: Spec
 spec = do
-    describe "Testing CryptoRNG" $ do
-      it "Genering random bytes wrapped in a newtype" testRandomBytes
-      it "Generating a random number within a range from Reader" testRandomNumber
-
----
-
-testRandomNumber :: Expectation
-testRandomNumber = runEff $ do
-  cryptoState <- newCryptoRNGState
-  void $ runCryptoRNG cryptoState
-            . runReader ((10, 20) :: (Int, Int))
-            $ generatingRandomNumber
-
-generatingRandomNumber :: (CryptoRNG :> es, Reader (Int, Int) :> es) => Eff es Int
-generatingRandomNumber = do
-  bounds <- ask
-  randomR bounds
-
----
-
-testRandomBytes :: Expectation
-testRandomBytes = runEff $ do
-  cryptoState <- newCryptoRNGState
-  void $ runCryptoRNG cryptoState generateUID
-
-newtype UID = UID ByteString
-  deriving newtype (Show, Eq, Ord)
-
-generateUID :: (CryptoRNG :> es ) => Eff es UID
-generateUID = do
-  bytes <- randomBytes 8
-  pure $ UID bytes
+  describe "Testing CryptoRNG" $ do
+    Dynamic.spec
+    Static.spec
